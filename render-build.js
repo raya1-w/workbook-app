b2fdae833bd9f6a512e918600d0ea15caa2a2e1d
raya1-w <rray0134567@gmail.com>
2025-05-03 03:28:12 +0530

    render-build.js

--- render-build.js
+++ render-build.js
@@ -1,5 +1,5 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
+import { execSync } from 'child_process';
+import fs from 'fs';
 
 console.log('Starting custom build script for Render deployment...');
 
