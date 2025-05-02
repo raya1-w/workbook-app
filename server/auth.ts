50e625902907e0a8be8a0c45848279dd2d9a092d
raya1-w <rray0134567@gmail.com>
2025-05-03 05:15:13 +0530

    Update auth.ts

--- server/auth.ts
+++ server/auth.ts
@@ -53,6 +53,7 @@ export function setupAuth(app: Express) {
     cookie: {
       secure: process.env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     },
   };
 
